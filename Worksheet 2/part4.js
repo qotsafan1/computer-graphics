3975b8e82fc4afc07e57fd0432d9cadc014f2cc4
qotsafan1 <sigurbjornjonss@gmail.com>
2017-09-23 21:15:58 +0200

    finished worksheet 2

--- Worksheet 2/part4.js
+++ Worksheet 2/part4.js
@@ -3,16 +3,22 @@ var gl;
 var colorIndex = 0;
 var pointIndex = 0;
 var triangleIndex = 501;
+var circleIndex = 1002;
 
 var pointArray = [];
 var triangleArray = [];
+var circleArray = [];
 
 var maxNumTriangles = 1503;
 var maxNumVertices  = 3 * maxNumTriangles;
 
+var verticesInCircle = 102;
+var circleCount = 0; 
+
 // Mode
-// 0 - Point
+// 0 - Point,
 // 1 - Triangle
+// 2 - Circle
 var mode = 0;
 
 var colors = [
@@ -84,6 +90,35 @@ window.onload = function init()
             else {
                 createPoint(pointBuffer, colorBuffer, point);
             }
+        } else if (mode == 2) {
+            circleIndex++;
+
+            if (circleIndex%2 === 0) {
+                circleCount++;
+
+                var midPoint = pointArray.pop();
+                var outPoint = point;
+                
+                pointIndex--;
+
+                circleArray.push(midPoint);
+                circleArray.push(outPoint);
+                
+                //Calculate radius
+                var radius = Math.hypot(outPoint[0]-midPoint[0], outPoint[1]-midPoint[1]);
+
+                var circlePoints = createCirclePoints( midPoint, radius, verticesInCircle );
+                for (var i=0; i<circlePoints.length; i++) {
+                    gl.bindBuffer( gl.ARRAY_BUFFER, pointBuffer );
+                    gl.bufferSubData(gl.ARRAY_BUFFER, 8*(1002+i+(verticesInCircle*(circleCount-1))), flatten(circlePoints[i]));
+                    
+                    t = vec4(colors[colorIndex]);
+                    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+                    gl.bufferSubData(gl.ARRAY_BUFFER, 16*(1002+i+(verticesInCircle*(circleCount-1))), flatten(t));
+                }
+            } else {
+                createPoint(pointBuffer, colorBuffer, point);
+            }
         } else {
             createPoint(pointBuffer, colorBuffer, point);
         }
@@ -104,6 +139,10 @@ function render()
     if (triangleIndex > 501) {
         gl.drawArrays( gl.TRIANGLES, 501, triangleArray.length+3);
     }
+
+    for (var i = 0; i < circleCount; i++) {
+        gl.drawArrays(gl.TRIANGLE_FAN, 1002 + verticesInCircle*i, verticesInCircle);
+    }
     
     window.requestAnimFrame(render);
 }
@@ -111,6 +150,8 @@ function render()
 function clearCanvas() {
     pointIndex = 0;
     triangleIndex = 501;
+    circleIndex = 1002;
+    circleCount = 0;
     pointArray = [];
     triangleArray = [];
 
@@ -120,10 +161,12 @@ function clearCanvas() {
 function changeMode(newMode) {
     if (mode === 1 && triangleIndex%3 !== 0 && triangleIndex !== 501) {
         triangleIndex--;
-
+        
         if (triangleIndex%3 !== 0) {
             triangleIndex--;
         }
+    } else if (mode === 2 && circleIndex%2 !== 0 && circleIndex !== 1002) {
+        circleIndex--;
     }
     
     mode = newMode;
@@ -152,4 +195,23 @@ function createTriangle(pointBuffer, colorBuffer, points) {
     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(triangleIndex+0), flatten(t));
     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(triangleIndex+1), flatten(t));
     gl.bufferSubData(gl.ARRAY_BUFFER, 16*(triangleIndex+2), flatten(t));
+}
+
+function createCirclePoints( cent, rad, k )
+{
+    var circlePoints = [];
+    circlePoints.push(cent);
+    for (i = 0; i <= k-3; i++){
+        circlePoints.push(vec2(
+            rad*Math.cos(i*2*Math.PI/k) + cent[0],
+            rad*Math.sin(i*2*Math.PI/k) + cent[1] 
+        ));
+    }
+
+    circlePoints.push(vec2(
+        rad*Math.cos(0) + cent[0],
+        rad*Math.sin(0) + cent[1] 
+    ));
+
+    return circlePoints;
 }
