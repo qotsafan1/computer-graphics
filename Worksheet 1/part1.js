463c7ea0740a9f0485ad5527989ef78d7584e774
qotsafan1 <sigurbjornjonss@gmail.com>
2017-11-26 17:54:18 +0100

    worksheet 1 totally finished

--- Worksheet 1/part1.js
+++ Worksheet 1/part1.js
@@ -1,14 +1,17 @@
+var gl;
+var canvas;
+
 window.onload = function init()
 {
-    canvas = document.getElementById( "webgl" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { 
-        alert( "WebGL isn't available" ); 
+    canvas = document.getElementById("webgl");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        console.log("WebGL isn't available");
         return;
     }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
 
     render();
@@ -17,5 +20,5 @@ window.onload = function init()
 
 function render()
 {
-    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
--- Worksheet 1/part5.js
+++ Worksheet 1/part5.js
@@ -1,40 +1,39 @@
 var gl;
-var numCirclePoints = 4000;
-var radius = 0.25;
-var centerX = 0.0;
-var centerY = -0.65;
-var dry = 0.1;
-var speed = 0.02
+var canvas
 var direction = 1.0;
+
+var theta = 0.0
+var thetaLoc;
 
 var points = [];
 
 window.onload = function init()
 {
-    canvas = document.getElementById( "webgl" );
-    
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { 
-        console.log( "WebGL isn't available" ); 
+    canvas = document.getElementById("webgl");
+
+    gl = WebGLUtils.setupWebGL(canvas);
+    if (!gl) {
+        console.log("WebGL isn't available");
         return;
     }
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(0.3921, 0.5843, 0.9294, 1.0);
 
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
-    points.push(vec2(centerX, centerY));
-    createCirclePoints(vec2(centerX, centerY), radius, numCirclePoints );
+    createCirclePoints(0.25, 400);
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-    
+
     var vPosition = gl.getAttribLocation(program, "vPosition");
     gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
 
     render();
 };
@@ -42,41 +41,24 @@ window.onload = function init()
 
 function render()
 {
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLE_FAN, 0, points.length );
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-    moveCircle(dry);
+    if (theta > 0.8 || theta < -0.8) {
+        direction *= -1;
+    }
+
+    theta += 0.009 * direction;
+    gl.uniform1f(thetaLoc, theta);
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, points.length);
+
     window.requestAnimFrame(render);
-
-    if (dry > 1.4 && direction > 0) {
-        direction = -1.0;
-    } else if (dry < -0.1 && direction < 0) {
-        direction = 1.0;
-    }
-
-    dry = dry + (speed * direction);
 }
 
-function createCirclePoints( cent, rad, k )
+function createCirclePoints(radius, numberOfPoints)
 {
-    var dAngle = 2*Math.PI/k;
-    for( i=k; i>=0; i-- ) {
-        a = i*dAngle;
-        var p = vec2( rad*Math.sin(a) + cent[0], rad*Math.cos(a) + cent[1] );
-        points.push(p);
-        i--;
-        var p = vec2( rad*Math.sin(a) + cent[0], rad*Math.cos(a) + cent[1] );
-        points.push(p);
-        i--;
+    for (var i = 0; i <= numberOfPoints; i++) {
+        var point = vec2(radius * Math.cos((2 * Math.PI * i) / numberOfPoints), radius * Math.sin((2 * Math.PI * i) / numberOfPoints));
+        points.push(point);
     }
-}
-
-function moveCircle(dry) {
-    points = [];
-
-    points.push(vec2(centerX, (centerY+dry)));
-
-    createCirclePoints(vec2(centerX, (centerY+dry)), radius, numCirclePoints );
-        
-    gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(points));
 }
